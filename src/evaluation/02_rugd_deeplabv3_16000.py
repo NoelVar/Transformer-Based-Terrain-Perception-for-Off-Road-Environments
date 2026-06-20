b4b6b3ebeb71723c2ab0246e51707f8b41482bd8
NoelVar <varnoel11@gmail.com>
2026-06-20 13:49:38 +0100

    Added DeepLabV3 - RUGD 16k and it's analysis

---
+++ src/evaluation/02_rugd_deeplabv3_16000.py
@@ -0,0 +1,22 @@
+# Ref: https://mmsegmentation.readthedocs.io/en/latest/user_guides/3_inference.html
+from mmseg.apis import init_model, inference_model, MMSegInferencer
+
+img_list = [
+    "creek/creek_00181.png",
+    "park-1/park-1_00761.png",
+    "park-2/park-2_00136.png",
+    "park-8/park-8_00111.png",
+    "trail/trail_00221.png",
+    "trail-7/trail-7_00001.png",
+    "trail-11/trail-11_00011.png",
+    "trail-11/trail-11_01856.png",
+    "trail-14/trail-14_00291.png",
+    "village/village_00578.png",
+]
+
+config = "../../results/rugd_test/deeplabv3plus_r50-d8_4xb2-40k_rugd.py"
+checkpoint = "../../results/rugd_test_continue/iter_10000.pth"
+
+inferencer = MMSegInferencer(model=config, weights=checkpoint, device="cpu")
+for data in img_list:
+    inferencer(f"../../data/processed/rugd/images/{data}", out_dir="../../results/visualisations_16k/")
